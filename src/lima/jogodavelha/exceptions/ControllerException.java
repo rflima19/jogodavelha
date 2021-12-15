9e8d672862b14a26ba4ea29bf82ac17d1fc85560
Rafael Lima <rafaellima02929@gmail.com>
2021-12-15 15:44:10 -0300

    Cria classes de exceção

---
+++ src/lima/jogodavelha/exceptions/ControllerException.java
@@ -0,0 +1,30 @@
+package lima.jogodavelha.exceptions;
+
+public class ControllerException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6235370562435753757L;
+
+	public ControllerException() {
+		super();
+	}
+
+	public ControllerException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ControllerException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ControllerException(String message) {
+		super(message);
+	}
+
+	public ControllerException(Throwable cause) {
+		super(cause);
+	}
+
+}
---
+++ src/lima/jogodavelha/exceptions/DAOException.java
@@ -0,0 +1,30 @@
+package lima.jogodavelha.exceptions;
+
+public class DAOException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 611661389883885573L;
+
+	public DAOException() {
+		super();
+	}
+
+	public DAOException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public DAOException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DAOException(String message) {
+		super(message);
+	}
+
+	public DAOException(Throwable cause) {
+		super(cause);
+	}
+
+}
