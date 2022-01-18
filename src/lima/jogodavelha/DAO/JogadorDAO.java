ef681cb269a6113a1d5419c74fe97c557933a3de
Rafael Lima <rafaellima02929@gmail.com>
2022-01-18 15:39:48 -0300

    Refatoração do repositório para nova API de I/O Java, Correção do método salvar pontuação de empate

--- src/lima/jogodavelha/DAO/JogadorDAO.java
+++ src/lima/jogodavelha/DAO/JogadorDAO.java
@@ -1,13 +1,18 @@
 package lima.jogodavelha.DAO;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.Reader;
 import java.io.Writer;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiPredicate;
@@ -17,19 +22,24 @@ import lima.jogodavelha.exceptions.DAOException;
 import lima.jogodavelha.model.Jogador;
 
 public class JogadorDAO implements JogoDaVelhaDAO {
-
-	public static final String USE_HOME = System.getProperty("user.home");
-	public static final File DIRETORIO = new File(JogadorDAO.USE_HOME + File.separator + "files" + File.separator);
-	public static final File ARQUIVO = new File(JogadorDAO.DIRETORIO, "jogo.txt");
+	
+	public static final String SEPARATOR = FileSystems.getDefault().getSeparator();
+	public static final Path USE_HOME = Paths.get(System.getProperty("user.home") + JogadorDAO.SEPARATOR);
+	public static final Path DIRETORIO = JogadorDAO.USE_HOME.resolve("files" + JogadorDAO.SEPARATOR);
+	public static final Path ARQUIVO = JogadorDAO.DIRETORIO.resolve("jogo.txt");
 
 	public synchronized void cadastarJogador(Jogador jogador) throws DAOException {
+
 		try {
-			this.criarBaseDados();
+			this.criarArquivos();
 		} catch (IOException e) {
 			throw new DAOException("Não foi possivel criar um novo arquivo no sistema", e);
 		}
 
-		try (Writer out = new FileWriter(JogadorDAO.ARQUIVO, true); PrintWriter print = new PrintWriter(out)) {
+		try (Writer out = Files.newBufferedWriter(JogadorDAO.ARQUIVO, Charset.defaultCharset(),
+					StandardOpenOption.WRITE, 
+					StandardOpenOption.APPEND); 
+				PrintWriter print = new PrintWriter(out)) {
 			String nome = (jogador.getNome().length() > 20) ? jogador.getNome().substring(0, 20) : jogador.getNome();
 			String parse = nome + ";" + jogador.getQuantidadeVitorias() + ";" + jogador.getQuantidadeDerrotas() + ";"
 					+ jogador.getQuantidadeEmpates(); // nome_jogador;num_vitorias;num_derrotas;num_empates
@@ -44,15 +54,18 @@ public class JogadorDAO implements JogoDaVelhaDAO {
 		List<Jogador> lista = new ArrayList<>();
 
 		try {
-			boolean isCriouArquivo = this.criarBaseDados();
+			boolean isCriouArquivo = this.criarArquivos();
 			if (isCriouArquivo == true) {
-				return lista; // criou a base de dados vazia, retorna lista vazia
+				return lista; // criou a base de dados vazia
 			}
 		} catch (IOException e) {
 			throw new DAOException("Não foi possivel criar um novo arquivo no sistema", e);
 		}
 
-		try (Reader in = new FileReader(JogadorDAO.ARQUIVO); BufferedReader buffer = new BufferedReader(in)) {
+		try (InputStream in = Files.newInputStream(JogadorDAO.ARQUIVO,
+					StandardOpenOption.READ); 
+				BufferedReader buffer = new BufferedReader(new InputStreamReader(in, 
+					Charset.defaultCharset()))) {
 			String line = null;
 			String[] tokens = null;
 			while ((line = buffer.readLine()) != null) {
@@ -65,11 +78,12 @@ public class JogadorDAO implements JogoDaVelhaDAO {
 		}
 		return lista;
 	}
-
+	
 	@Override
 	public synchronized Jogador pesquisarJogador(String nome) throws DAOException {
+
 		try {
-			boolean isCriouArquivo = this.criarBaseDados();
+			boolean isCriouArquivo = this.criarArquivos();
 			if (isCriouArquivo == true) {
 				return null; // criou a base de dados vazia
 			}
@@ -78,7 +92,10 @@ public class JogadorDAO implements JogoDaVelhaDAO {
 		}
 
 		Jogador jogador = null;
-		try (Reader in = new FileReader(JogadorDAO.ARQUIVO); BufferedReader buffer = new BufferedReader(in)) {
+		try (InputStream in = Files.newInputStream(JogadorDAO.ARQUIVO, 
+					StandardOpenOption.READ); 
+				BufferedReader buffer = new BufferedReader(new InputStreamReader(in, 
+						Charset.defaultCharset()))) {
 			String line = null;
 			String[] tokens = null;
 			String n = null;
@@ -103,15 +120,13 @@ public class JogadorDAO implements JogoDaVelhaDAO {
 			throw new IllegalArgumentException("Argumento jogador nulo");
 		}
 		this.salvarPontuacao((String s1, String s2) -> {
-					return s1.equals(s2);
-				}, 
-				(String[] array) -> {
-					int num = Integer.parseInt(array[1]);
-					++num;
-					String str = array[0] + ";" + num + ";" + array[2] + ";" + array[3];
-					return str;
-				}, 
-				jogador);
+			return s1.equals(s2);
+		}, (String[] array) -> {
+			int num = Integer.parseInt(array[1]);
+			++num;
+			String str = array[0] + ";" + num + ";" + array[2] + ";" + array[3];
+			return str;
+		}, jogador);
 	}
 
 	@Override
@@ -119,17 +134,15 @@ public class JogadorDAO implements JogoDaVelhaDAO {
 		if (jogador == null) {
 			throw new IllegalArgumentException("Argumento jogador nulo");
 		}
-		
+
 		this.salvarPontuacao((String s1, String s2) -> {
-					return s1.equals(s2);
-				}, 
-				(String[] array) -> {
-					int num = Integer.parseInt(array[2]);
-					++num;
-					String str = array[0] + ";" + array[1] + ";" + num + ";" + array[3];
-					return str;
-				}, 
-				jogador);
+			return s1.equals(s2);
+		}, (String[] array) -> {
+			int num = Integer.parseInt(array[2]);
+			++num;
+			String str = array[0] + ";" + array[1] + ";" + num + ";" + array[3];
+			return str;
+		}, jogador);
 	}
 
 	@Override
@@ -139,92 +152,82 @@ public class JogadorDAO implements JogoDaVelhaDAO {
 		} else if (jogador2 == null) {
 			throw new IllegalArgumentException("Argumento jogador2 nulo");
 		}
-		
+
 		this.salvarPontuacao((String s1, String s2) -> {
-					return s1.equals(s2);
-				}, 
-				(String[] array) -> {
-					int num = Integer.parseInt(array[3]);
-					++num;
-					String str = array[0] + ";" + array[1] + ";" + array[2] + ";" + num;
-					return str;
-				}, 
-				jogador1, jogador2);
+			return s1.equals(s2);
+		}, (String[] array) -> {
+			int num = Integer.parseInt(array[3]);
+			++num;
+			String str = array[0] + ";" + array[1] + ";" + array[2] + ";" + num;
+			return str;
+		}, jogador1, jogador2);
 	}
-	
-	private synchronized void salvarPontuacao(BiPredicate<String, String> predicate, Function<String[], String> function, Jogador... jogadores) throws DAOException {
+
+	private synchronized void salvarPontuacao(BiPredicate<String, String> predicate,
+			Function<String[], String> function, Jogador... jogadores) throws DAOException {
+		
 		try {
-			this.criarBaseDados();
+			boolean isCriouArquivo = this.criarArquivos();
+			if (isCriouArquivo == true) {
+				throw new DAOException("Base de dados vazia"); // criou a base de dados vazia
+			}
 		} catch (IOException e) {
 			throw new DAOException("Não foi possivel criar um novo arquivo no sistema", e);
 		}
-		if (JogadorDAO.ARQUIVO.length() == 0) {
-			throw new DAOException("Base de dados vazia");
-		}
-		File arquivoTemporario = new File(JogadorDAO.DIRETORIO, "jogo_temp.txt");
+
+		Path arquivoTemporario = null;
 		try {
-			arquivoTemporario.createNewFile();
+			arquivoTemporario = Files.createTempFile("jogo", "temp");
 		} catch (IOException e) {
 			throw new DAOException("Não foi possivel criar o arquivo temporário", e);
 		}
-		try {
-			try (Reader in = new FileReader(JogadorDAO.ARQUIVO);
-					BufferedReader buffer = new BufferedReader(in);
-					Writer out = new FileWriter(arquivoTemporario);
-					PrintWriter print = new PrintWriter(out)) {
-				String line = null;
-				String[] tokens = null;
-				String nome = null;
-				String str = null;
-				while ((line = buffer.readLine()) != null) {
-					tokens = line.split(";");
-					nome = tokens[0];
-					for (int i = 0; i < jogadores.length; i++) {
-						if (predicate.test(nome, jogadores[i].getNome()) == true) {
-							str = function.apply(tokens);
-						} else {
-							str = tokens[0] + ";" + tokens[1] + ";" + tokens[2] + ";" + tokens[3];
-						}
-						print.println(str);
-					}	
-				}
-			} catch (IOException e) {
-				throw new DAOException("Não foi possível ler a base de dados", e);
-			}
 
-			try {
-				JogadorDAO.this.copiarArquivo(arquivoTemporario, JogadorDAO.ARQUIVO);
-			} catch (IOException e) {
-				throw new DAOException("Não foi possível restaurar a base de dados", e);
+		try (InputStream in = Files.newInputStream(JogadorDAO.ARQUIVO,
+					StandardOpenOption.READ); 
+				BufferedReader buffer = new BufferedReader(new InputStreamReader(in));
+				Writer out = Files.newBufferedWriter(arquivoTemporario, 
+						Charset.defaultCharset(),
+						StandardOpenOption.WRITE);
+				PrintWriter print = new PrintWriter(out)) {
+			String line = null;
+			String[] tokens = null;
+			String nome = null;
+			String str = null;
+			while ((line = buffer.readLine()) != null) {
+				str = null;
+				tokens = line.split(";");
+				nome = tokens[0];
+				for (int i = 0; i < jogadores.length; i++) {
+					if (predicate.test(nome, jogadores[i].getNome()) == true) {
+						str = function.apply(tokens);
+						break;
+					}
+				}
+				if (str == null) {
+					str = tokens[0] + ";" + tokens[1] + ";" + tokens[2] + ";" + tokens[3];
+				}
+				print.println(str);
 			}
-		} finally {
-			if (arquivoTemporario.exists() == true) {
-				arquivoTemporario.delete();
-			}
+		} catch (IOException e) {
+			throw new DAOException("Não foi possível ler a base de dados", e);
+		}
+
+		try {
+			Files.copy(arquivoTemporario, JogadorDAO.ARQUIVO, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			throw new DAOException("Não foi possível restaurar a base de dados", e);
 		}
 	}
 
-	private synchronized boolean criarBaseDados() throws IOException {
+	private synchronized boolean criarArquivos() throws IOException {
 		boolean result = false;
-		if (JogadorDAO.ARQUIVO.exists() == false) {
-			if (JogadorDAO.DIRETORIO.exists() == false) {
-				JogadorDAO.DIRETORIO.mkdir();
+		if (Files.exists(JogadorDAO.ARQUIVO) == false) {
+			if (Files.exists(JogadorDAO.DIRETORIO) == false) {
+				Files.createDirectory(JogadorDAO.DIRETORIO);
 			}
-			JogadorDAO.ARQUIVO.createNewFile();
+			Files.createFile(JogadorDAO.ARQUIVO);
 			result = true;
 		}
 		return result;
-	}
-	
-	private synchronized void copiarArquivo(File fonte, File destino) throws IOException {
-		try (Reader in = new FileReader(fonte);
-				BufferedReader buffer = new BufferedReader(in);
-				Writer out = new FileWriter(destino);
-				PrintWriter print = new PrintWriter(out)) {
-			String line = null;
-			while ((line = buffer.readLine()) != null) {
-				print.println(line);
-			}
-		}
 	}
 }
